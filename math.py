9bcc53e138fbce8e059c5f015505fda3b760c9e6
Mitwaldhamne <62065544+Mitwaldhamne@users.noreply.github.com>
2022-02-01 23:42:20 +0530

    Creating math.py

---
+++ math.py
@@ -0,0 +1,15 @@
+#This is beautiful
+#For Bug456
+def add(x,y):#new add
+    pass
+#For master
+def sub(x,y):#new sub
+    pass
+#For rebasing master
+def multiply(x,y):#new mul
+    pass
+#For rebasing Bug
+def divide(x,y):#new div
+    return x/y
+#HAHaHa for Rebasing Bug
+##Stashing 
